759f54797b1e902fc7624ccd93d515a0ee16758f
Brazilly <vasilybarkov@mail.ru>
2019-09-10 12:46:50 +0300

    corrections, content

--- gulpfile.js
+++ gulpfile.js
@@ -73,9 +73,9 @@ gulp.task("images", function () {
 });
 
 gulp.task("webp", function () {
-  return gulp.src(["source/img/**/photo-*.{png,jpg}", "source/img/**/panorama-*.{png,jpg}"])
+  return gulp.src(["source/img/interiors/*.{png,jpg}"])
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("source/img/interiors"));
 });
 
 gulp.task("sprite", function () {
